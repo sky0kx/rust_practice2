5f0ff2e8c818aaf7465086637b3a73a98228ad4f
Sky <93914899+sky0kx@users.noreply.github.com>
2026-03-10 16:05:42 +0300

    edit3

--- hackerrank/task3.rs
+++ hackerrank/task3.rs
@@ -1,1 +1,8 @@
-
+fn staircase(n: i32) {
+    for i in 1..=n {
+        let spaces = n - i;
+        let hashes = i;
+        let line = " ".repeat(spaces as usize) + &"#".repeat(hashes as usize);
+        println!("{}", line);
+    }
+}
