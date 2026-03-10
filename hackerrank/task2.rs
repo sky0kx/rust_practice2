f4f024c3b15842d8ff237c275e93bdf7caf69153
Sky <93914899+sky0kx@users.noreply.github.com>
2026-03-10 15:59:24 +0300

    edit2

--- hackerrank/task2.rs
+++ hackerrank/task2.rs
@@ -1,1 +1,12 @@
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    let n = arr.len();
+    let mut primary_sum = 0;
+    let mut secondary_sum = 0;
 
+    for i in 0..n {
+        primary_sum += arr[i][i];
+        secondary_sum += arr[i][n - 1 - i];
+    }
+
+    (primary_sum - secondary_sum).abs()
+}
