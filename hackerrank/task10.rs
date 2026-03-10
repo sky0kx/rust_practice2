ea0d365481936fac7f14dd32605b3afbadf369c9
Sky <93914899+sky0kx@users.noreply.github.com>
2026-03-10 16:59:42 +0300

    edit10

--- hackerrank/task10.rs
+++ hackerrank/task10.rs
@@ -1,1 +1,19 @@
+fn migratoryBirds(arr: &[i32]) -> i32 {
+    let mut count = [0; 5];
 
+    for &id in arr {
+        count[(id - 1) as usize] += 1;
+    }
+
+    let mut max_count = 0;
+    let mut result = 0;
+
+    for i in 0..5 {
+        if count[i] > max_count {
+            max_count = count[i];
+            result = i + 1;
+        }
+    }
+
+    result as i32
+}
