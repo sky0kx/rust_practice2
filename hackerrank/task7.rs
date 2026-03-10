954a975b6904bc6a8bd3be9e4350a4e5e8dfc77e
Sky <93914899+sky0kx@users.noreply.github.com>
2026-03-10 16:43:38 +0300

    edit7

--- hackerrank/task7.rs
+++ hackerrank/task7.rs
@@ -1,1 +1,15 @@
+fn getTotalX(a: &[i32], b: &[i32]) -> i32 {
+    let start = *a.iter().max().unwrap();
+    let end = *b.iter().min().unwrap();
 
+    let mut count = 0;
+
+    for x in start..=end {
+        if a.iter().all(|&ai| x % ai == 0) &&
+           b.iter().all(|&bi| bi % x == 0) {
+            count += 1;
+        }
+    }
+
+    count
+}
