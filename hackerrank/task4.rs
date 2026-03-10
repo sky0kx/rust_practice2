c3d60eeb2a16393007b7625ee36205922471384b
Sky <93914899+sky0kx@users.noreply.github.com>
2026-03-10 16:24:22 +0300

    edit4

--- hackerrank/task4.rs
+++ hackerrank/task4.rs
@@ -1,1 +1,14 @@
-
+fn grading_students(grades: &[i32]) -> Vec<i32> {
+    grades.iter().map(|&grade| {
+        if grade < 38 {
+            grade
+        } else {
+            let next_mult_5 = ((grade / 5) + 1) * 5;
+            if next_mult_5 - grade < 3 {
+                next_mult_5
+            } else {
+                grade
+            }
+        }
+    }).collect()
+}
